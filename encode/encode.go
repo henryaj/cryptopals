f746526e4f45106f1c16c01fbeadabeccef5c5f6
Henry Stanley <hstanley@pivotal.io>
2018-04-09 18:20:48 +0100

    set 1, challenge <= 4
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>

--- encode/encode.go
+++ encode/encode.go
@@ -5,6 +5,8 @@ import (
 	"encoding/base64"
 )
 
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 func HexStringToBytes(input string) []byte {
 	b, err := hex.DecodeString(input)
 	if err != nil {
@@ -25,11 +27,27 @@ func HexBytesToString(hexBytes []byte) string {
 func FixedXOR(input, xor []byte) []byte {
 	dest := make([]byte, len(input))
 
-	for i, _ := range input {
+	for i := range input {
 		b := input[i] ^ xor[i]
 		dest[i] = b
 	}
 
 	return dest
+}
+
+func SingleByteXOR(input []byte) [][]byte {
+	var result [][]byte
+
+	for _, xorTarget := range chars {
+		xorTargetByte := byte(xorTarget)
+		dest := make([]byte, len(input))
+		for i := range input {
+			dest[i] = input[i] ^ xorTargetByte
+		}
+
+		result = append(result, dest)
+	}
+
+	return result
 }
 
--- encode/encode_test.go
+++ encode/encode_test.go
@@ -12,8 +12,24 @@ var _ = Describe("FixedXOR", func() {
 		input1 := HexStringToBytes("1c0111001f010100061a024b53535009181c")
 		input2 := HexStringToBytes("686974207468652062756c6c277320657965")
 
-		expected := HexStringToBytes("746865206b696420646f6e277420706c617")
+		expected := HexStringToBytes("746865206b696420646f6e277420706c6179")
 
 		Expect(FixedXOR(input1, input2)).To(Equal(expected))
 	})
 })
+
+var _ = Describe("SingleByteXOR", func() {
+	It("returns all possible outputs from XORing a string against a single byte", func() {
+		input := HexStringToBytes("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+
+		results := SingleByteXOR(input)
+		Expect(slicesToString(results)).To(ContainElement("Cooking MC's like a pound of bacon"))
+	})
+})
+
+func slicesToString(input [][]byte) (result []string) {
+	for _, slice := range input {
+		result = append(result, string(slice))
+	}
+	return result
+}
